085e78468d03066dca6ce967459233830f07ec93
Greg Siano <gsiano@box.com>
2015-05-11 01:31:04 -0400

    model

--- model.py
+++ model.py
@@ -1,10 +1,23 @@
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import Perceptron, LinearRegression
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import Perceptron, LinearRegression, Lasso, Ridge, ElasticNet
+from sklearn.ensemble import GradientBoostingRegressor, AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 import pandas as pd
 import datetime
 import numpy as np
+import math
+
+regressions = dict()
+regressions["LR"] = LinearRegression()
+regressions["KNN"] = KNeighborsRegressor(n_neighbors=5)
+regressions["PR"] = Perceptron()
+regressions["LS"] = Lasso()
+regressions["RI"] = Ridge()
+regressions["EN"] = ElasticNet()
+regressions["GBR"] = GradientBoostingRegressor()
+regressions["AB"] = AdaBoostRegressor()
+regressions["DT"] = DecisionTreeRegressor()
 
 WT = ('HZ', 'FU', 'BLSN', 'VCTS', 'DZ', 'BR', 'FG', 'BCFG', 'DU',
       'FZRA', 'TS', 'RA', 'PL', 'GR', 'FZDZ', 'VCFG', 'FG+', 'TSRA',
@@ -39,31 +52,10 @@ def date_to_doy(date):
     date = datetime.datetime(int(y), int(m), int(d)).date()
     return date.timetuple().tm_yday
 
-def do_LR(x, y, testx):
-    print "LR.."
-    reg = LinearRegression()
+def do_regression(r, x, y, testx):
+    reg = regressions[r]
     reg.fit(x, y)
-    print "done fitting"
     return reg.predict(testx)
-
-def do_gbr(x, y, testx):
-    print "gbr.."
-    reg = GradientBoostingRegressor(n_estimators=20)
-    reg.fit(x, y)
-    print "done fitting"
-    return reg.predict(testx)
-
-def do_knn(x, y, testx, n):
-    knnc = KNeighborsClassifier(n_neighbors=n)
-    knnc.fit(x, y)
-    return clf.predict(testx)
-
-def do_perceptron(x, y, testx):
-    print "perceptron.."
-    clf = Perceptron()
-    clf.fit(x, y)
-    print "done fitting"
-    return clf.predict(testx)
 
 def set_to_dict(set):
     d = dict()
@@ -90,29 +82,13 @@ for t in WT:
     train[t] = train['codesum'].map(lambda x: t in str(x))
 for t in WT:
     test[t] = test['codesum'].map(lambda x: t in str(x))
-"""departs = set_to_dict(list(set(train.depart.values)))
-codesums = set_to_dict(list(set(train.codesum.values)))
-sunrises = set_to_dict(list(set(train.sunrise.values)))
-train['depart'] = train['depart'].apply(lambda x: str_to_int(str(x), departs))
-train['codesum'] = train['codesum'].apply(lambda x: str_to_int(str(x), codesums))
-train['sunrise'] = train['sunrise'].apply(lambda x: str_to_int(str(x), sunrises))"""
-#units = pd.concat([train["units"], train["store_nbr"]], axis=1, keys=["units", "store_nbr"])
 units = train["units"]
 results = test["units"]
 train = train.drop("codesum", 1)
 test = test.drop("codesum", 1)
-"""test = pd.read_csv("data/test_features.csv")
-test = test.drop("sunrise", 1)
-test = test.drop("depart", 1)
-test = test.drop("codesum", 1)
-test = test.drop("date", 1)"""
-#test.fillna(0, inplace=True)
 
-#a = do_perceptron(train, units, test)
-#a = do_gbr(train,units,test)
-#b = np.sqrt(mean_squared_error(a, results))
-
-# for each test point, train only on store_nbr
+# train by store
+MSE = 0.0
 for i in range(1, 46):
     traindata = train[train.store_nbr == i]
     unitdata = traindata["units"]
@@ -122,12 +98,11 @@ for i in range(1, 46):
     testdata.drop("units", 1)
     items = list(set(testdata.item_nbr.values))
 
-    a = do_gbr(traindata,unitdata,testdata)
-    print "%d: " % i
-    print np.sqrt(mean_squared_error(a,resultdata))
+    a = do_regression("GBR", traindata,unitdata,testdata)
+    MSE += mean_squared_error(a, resultdata)
     tot = 0.0
     resultdata = resultdata.values
     for j in range(0, len(a)):
         tot += abs(a[j] - resultdata[j])
-        print "(%d %d)"% (a[j], resultdata[j]),
-    print tot
+    print "absolute error for store %d: %d" % (i, tot)
+print "RMSE: %d" % math.sqrt(MSE)
